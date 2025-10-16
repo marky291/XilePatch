3beb009b3b216a2dcb8e5528dbd644cee463335d
achenxu <alexischenxu@gmail.com>
2025-10-15 23:54:44 -0700

    October 2025

---
+++ Thor Patcher/Thor Patcher/Patcher/Scripts/main.js
@@ -0,0 +1,4 @@
+//Note: Currently the patcher does not minify the code before pack.
+//You will have to do it manually if you wish to reduce the footprint.
+
+require('test.js');
---
+++ Thor Patcher/Thor Patcher/Skins/v1/Readme.txt
@@ -0,0 +1,2 @@
+This is the default skin used in older versions of Thor Patcher.
+It has been upgrade to support 2.6
